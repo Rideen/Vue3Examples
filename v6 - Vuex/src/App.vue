ed8605b6fa475fd92563843e0128e8be0c6ba2ab
Rideen <narancic.rideen@gmail.com>
2020-12-09 20:41:01 +0100

    Vuex mutations

--- v6 - Vuex/src/App.vue
+++ v6 - Vuex/src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <base-container title="Vuex">
     <the-counter></the-counter>
-  <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    ChangeCounter,
   },
   methods: {
     addOne() {
-      this.$store.state.counter++;
-    }
-  }
+      this.$store.commit('increase', { value: 10 });
+      // ==== OR ==== 
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 10
+      // });
+    },
+  },
 };
 </script>
 
--- v6 - Vuex/src/components/TheCounter.vue
+++ v6 - Vuex/src/components/TheCounter.vue
@@ -1,13 +1,13 @@
 <template>
-    <h3>{{ counter }}</h3>
+  <h3>{{ counter }}</h3>
 </template>
 
 <script>
 export default {
-    computed: {
-        counter() {
-        return this.$store.state.counter;
-        }
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
   },
-}
+};
 </script>
