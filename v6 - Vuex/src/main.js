cac876a905b918bb505f7ee2983afcd05b5accdb
Rideen <narancic.rideen@gmail.com>
2020-12-10 19:03:54 +0100

    Vuex - getters example

--- v6 - Vuex/src/main.js
+++ v6 - Vuex/src/main.js
@@ -4,19 +4,37 @@ import { createStore } from 'vuex';
 import App from './App.vue';
 
 const store = createStore({
-    state() {
-        return {
-            counter: 0
-        }
-    },
-    mutations: {
-        increment(state) {
-            state.counter += 2;
-        },
-        increase(state, payload) {
-            state.counter = state.counter + payload.value;
-        }
-    }
+	state() {
+		return {
+			counter: 0
+		}
+	},
+	mutations: {
+		increment(state) {
+			state.counter += 2;
+		},
+		increase(state, payload) {
+			state.counter = state.counter + payload.value;
+		}
+	},
+	getters: {
+		finalCounter(state) {
+			return state.counter * 2;
+		},
+		normalizedCounter(_, getters) {
+			const finalCounter = getters.finalCounter;
+			
+			if (finalCounter < 0) {
+				return 0;
+			}
+
+			if (finalCounter > 100) {
+				return 100;
+			}
+
+			return finalCounter;
+		}
+	}
 });
 
 const app = createApp(App);
