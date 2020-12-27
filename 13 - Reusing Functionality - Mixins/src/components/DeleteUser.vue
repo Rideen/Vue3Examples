f087df1bc7532727f74b30702d604400f5b9c18a
Rideen <narancic.rideen@gmail.com>
2020-12-27 13:55:02 +0100

    13 - Mixin merging

--- 13 - Reusing Functionality - Mixins/src/components/DeleteUser.vue
+++ 13 - Reusing Functionality - Mixins/src/components/DeleteUser.vue
@@ -1,5 +1,5 @@
 <template>
-  <user-alert v-if="alertIsVisible" title="Delete the User?" @close="hideAlert">
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
     <p>Do you want to continue with deleting a user?</p>
   </user-alert>
   <section>
@@ -13,9 +13,15 @@ import UserAlert from './UserAlert.vue';
 import alertMixin from '../mixins/alert.js';
 
 export default {
-   components: {
+  components: {
     UserAlert,
   },
-  mixins: [alertMixin]
+  data() {
+    return {
+      alertTitle: 'Delete user?',
+      // alertIsVisible: true // component options win if same properties are found in mixin and component :)
+    };
+  },
+  mixins: [alertMixin],
 };
 </script>
